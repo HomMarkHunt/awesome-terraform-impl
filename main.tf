c34bc63fdcb5f12210b435623d52fdbfd4923b56
HomMarkHunt <honmaso@gmail.com>
2020-12-01 05:32:51 +0900

    add tfenv

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_version = "0.13.5"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+  backend "s3" {
+    bucket  = "honmarkhunt-sample-tfstate"
+    region  = "ap-northeast-1"
+    key     = "terraform.tfstate"
+    encrypt = true
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
+resource "aws_vpc" "example" {
+          cidr_block = "10.0.0.0/16"
+}
